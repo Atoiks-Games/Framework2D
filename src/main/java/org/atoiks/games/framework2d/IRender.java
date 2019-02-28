80a2e5d212d1dde052f761f6d03c8b05694a105a
Paul Teng <plankp@outlook.com>
2019-02-28 06:17:24 -0500

    Remove IRender and IUpdate

--- src/main/java/org/atoiks/games/framework2d/IRender.java
+++
@@ -1,6 +0,0 @@
-package org.atoiks.games.framework2d;
-
-public interface IRender {
-
-    public void render(IGraphics g);
-}
--- src/main/java/org/atoiks/games/framework2d/IUpdate.java
+++
@@ -1,6 +0,0 @@
-package org.atoiks.games.framework2d;
-
-public interface IUpdate {
-
-    public void update(float dt);
-}
