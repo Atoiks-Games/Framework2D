61e263d3dd77e0afddb5e8b0ff811de2fbed6f8f
Paul Teng <plankp@outlook.com>
2019-05-27 12:30:28 -0400

    Add replace and load default

--- src/main/java/org/atoiks/games/framework2d/ResourceManager.java
+++ src/main/java/org/atoiks/games/framework2d/ResourceManager.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 
+import java.util.function.Supplier;
+
 import org.atoiks.games.framework2d.decoder.DecodeException;
 import org.atoiks.games.framework2d.decoder.IResourceDecoder;
 
@@ -42,6 +44,21 @@ public final class ResourceManager {
         return reload(path, resolver, decoder);
     }
 
+    public static <T> T loadOrDefault(final String path, final IPathResolver resolver, final IResourceDecoder<? extends T> decoder, final Supplier<? extends T> defaultValue) throws DecodeException {
+        final T existing = get(path);
+        if (existing != null) {
+            return existing;
+        }
+
+        try {
+            return reload(path, resolver, decoder);
+        } catch (DecodeException ex) {
+            final T val = defaultValue.get();
+            CACHE.put(path, val);
+            return val;
+        }
+    }
+
     public static <T> T load(final String path, final IResourceDecoder<? extends T> decoder) throws DecodeException {
         return load(path, InternalResourceResolver.INSTANCE, decoder);
     }
@@ -49,6 +66,10 @@ public final class ResourceManager {
     @SuppressWarnings("unchecked")
     public static <T> T get(final String path) {
         return (T) CACHE.get(path);
+    }
+
+    public static void replace(final String path, Object value) {
+        CACHE.replace(path, value);
     }
 
     public static void unload(final String path) {
