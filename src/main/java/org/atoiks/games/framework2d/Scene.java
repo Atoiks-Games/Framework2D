bb0f42208e9eb8b0ee240cf362aea4e413902bd7
Paul Teng <plankp@outlook.com>
2019-05-28 07:26:55 -0400

    Extract scene interface

--- src/main/java/org/atoiks/games/framework2d/Scene.java
+++ src/main/java/org/atoiks/games/framework2d/Scene.java
@@ -2,75 +2,45 @@ package org.atoiks.games.framework2d;
 
 import java.io.Serializable;
 
-public abstract class Scene implements Serializable {
+public abstract class Scene implements IScene, Serializable {
 
     private static final long serialVersionUID = -8839650470492997448L;
 
     protected transient SceneManager scene;
 
+    protected Scene() {
+    }
+
     final void attachSceneManager(SceneManager mgr) {
         this.scene = mgr;
     }
 
-    /**
-     * Renders on the screen
-     */
-    public abstract void render(IGraphics g);
-
-    /**
-     * Updates the entities in the scene
-     *
-     * @param dt elapsed time measured in seconds
-     * @return true if game should continue
-     */
-    public abstract boolean update(float dt);
-
-    /**
-     * Called when screen is resized
-     *
-     * @param x new width in pixels
-     * @param y new height in pixels
-     */
-    public abstract void resize(int x, int y);
-
-    /**
-     * Should stay constant during an object's lifetime!
-     *
-     * @return simple name of the class by default
-     */
-    public String getId() {
-        return this.getClass().getSimpleName();
+    @Override
+    public void render(IGraphics g) {
     }
 
-    /**
-     * The initializer of the scene. Guarantee called before the scene is
-     * entered
-     */
+    @Override
+    public boolean update(float dt) {
+        return true;
+    }
+
+    @Override
+    public void resize(int x, int y) {
+    }
+
+    @Override
     public void init() {
-        // Does nothing
     }
 
-    /**
-     * The clean up method of the scene. Guarantee called when the game is
-     * shutting down
-     */
+    @Override
     public void deinit() {
-        // Does nothing
     }
 
-    /**
-     * Called during a scene transition, after leaving the previous scene
-     *
-     * @param from - the scene id of thr previous scene
-     */
+    @Override
     public void enter(String from) {
-        // Does nothing
     }
 
-    /**
-     * Called during a scene transition, before entering the next scene.
-     */
+    @Override
     public void leave() {
-        // Does nothing
     }
 }
